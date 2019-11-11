b20c26bc3c509be87d7d5288c2f27653bebfe519
Joshua McGehee <mcgehee.joshua@gmail.com>
2019-11-11 13:39:22 -0700

    made nav, added background image

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,42 @@
 import React from 'react'
-import { Button } from 'reactstrap'
+import { Container, Nav, Navbar, NavItem, NavLink, NavbarBrand } from 'reactstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+import { navItems } from './helpers/constants'
 import './App.css'
 
 const App = () => {
   return (
-    <Button color="danger">Danger!</Button>
+    <div id="intro">
+      <Navbar color="info" dark>
+        <Container>
+          <NavbarBrand href="#">Joshua McGehee</NavbarBrand>
+          <Nav className="mr-auto">
+            {
+              navItems.map(nav => (
+                <NavItem key={ `${ nav.title } + ${ nav.alt }` }>
+                  <NavLink
+                    href={ nav.href }
+                    alt={ nav.alt }
+                    key={ nav.title }
+                  >{ nav.title }</NavLink>
+                </NavItem>
+              ))
+            }
+          </Nav>
+          <Nav>
+            <NavItem>
+              <FontAwesomeIcon icon={ faLink } />
+            </NavItem>
+          </Nav>
+          <form className="form-inline">
+            <div className="md-form my-0">
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+            </div>
+          </form>
+        </Container>
+      </Navbar>
+    </div>
   )
 }
 export default App
---
+++ portfolio/src/helpers/constants.js
@@ -0,0 +1,24 @@
+const navItems = [
+  {
+    title: 'Home',
+    href:'/home',
+    atl:'home',
+  },
+  {
+    title: 'Bio',
+    href:'/bio',
+    atl:'bio',
+  },
+  {
+    title: 'Projects',
+    href:'/prjects',
+    atl:'projects',
+  },
+  {
+    title: 'Contact',
+    href:'/contact',
+    atl:'contact',
+  },
+]
+
+export { navItems }
